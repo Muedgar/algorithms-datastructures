e18620d9e8cdc45a3c23af8073e68c34e1daa8bb
MuEdgar <edgarmutangana@gmail.com>
2026-05-07 23:45:43 +0000

    more exercises

--- exercises/1_arrays.js
+++ exercises/1_arrays.js
@@ -1,36 +1,17 @@
 
-function rotate1DArray(nums, k) {
-    // handle edge case
-    if(nums.length === 0) return nums;
-
-    // 
-    let n = nums.length;
+function rotateByKSteps(nums, k) {
+    if (nums.length === 0) return nums;
+    const n = nums.length;
     k = k % n;
-    // if(n % k < k) return nums;
-    // rotate all array
+
     reverse(nums, 0, n - 1);
-    // rotate 0 to k - 1
     reverse(nums, 0, k - 1);
-    // rotate k to nums.length - 1
     reverse(nums, k, n - 1);
 
-    return nums;
+    return nums.join('  ');
 }
 
 function reverse(nums, left, right) {
- while (left<right) {
-    [nums[left], nums[right]] = [nums[right], nums[left]];
-    left++;
-    right--
- }
-
- return nums;
-}
-
-
-console.log(rotate1DArray([1,2,3,4,5,6,7], 3))
-
-function reverseTwo(nums, left, right) {
     while (left < right) {
         [nums[left], nums[right]] = [nums[right], nums[left]];
         left++;
@@ -40,23 +21,4 @@ function reverseTwo(nums, left, right) {
     return nums;
 }
 
-
-
-
-
-
-
-
-function rotateArray(nums, k) {
-    const n = nums.length;
-
-    if (n === 0) return nums;
-
-    k = k % n;
-
-    reverse(nums, 0, n - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, n - 1);
-
-    return nums;
-}
+console.log(rotateByKSteps([1,2,3,4,5,6,7], 3));
