47cda9255adf0d78002120ce2e7d09892ddd5416
MuEdgar <edgarmutangana@gmail.com>
2026-05-09 23:59:11 +0000

    more exercises

--- exercises/1_arrays.js
+++ exercises/1_arrays.js
@@ -22,3 +22,33 @@ function reverse(nums, left, right) {
 }
 
 console.log(rotateByKSteps([1,2,3,4,5,6,7], 3));
+
+
+function cyclicRotation(nums, k) {
+    if (nums.length === 0) return nums;
+
+    k =  k % nums.length;
+
+    let count = 0;
+    let n = nums.length;
+
+    for (let start = 0; count < n - 1; start++) {
+        let current = start;
+        let prev = nums[start];
+
+        do {
+            const next = ( current + k ) % n;
+            const temp = nums[next];
+            nums[next] = prev;
+
+            prev = temp;
+            current = next;
+            count++
+
+        } while (start !== current);
+    } 
+
+    return nums.join('  ');
+}
+
+console.log(cyclicRotation([1,2,3,4,5,6,7], 3));
